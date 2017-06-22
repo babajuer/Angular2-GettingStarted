c8420aeed1bfc4bdea2e029a3d427742a85f3e74
Bingjie FANG <bingjie.fang@kellydeli.com>
2017-06-22 19:23:13 +0200

    finish chapter 4

--- APM - StartFbj/app/app.component.ts
+++ APM - StartFbj/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started</h1>
-    `
+        <h1>Angular2: Getting Started fbj</h1>
+		<h3>
+		{{pageTItle}}
+		</h3>
+	`
 })
-export class AppComponent { }
+export class AppComponent {
+	pageTItle: string = `---haha this is title---`;
+ }
