dc1c6508d477796ed49afb626007c30484d1468f
Bingjie FANG <bingjie.fang@kellydeli.com>
2017-06-23 17:50:01 +0200

    finish6.3

--- APM - StartFbj/app/app.component.ts
+++ APM - StartFbj/app/app.component.ts
@@ -3,12 +3,15 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'pm-app',
     template: `
-        <h1>Angular2: Getting Started fbj</h1>
+        <h1>Angular2: Getting Started </h1>
 		<h3>
 		{{pageTItle}}
 		</h3>
+		 
+		<pm-productList></pm-productList>
+
 	`
 })
 export class AppComponent {
-	pageTItle: string = `---haha this is title---`;
+	pageTItle: string = `Product Management`;
  }
